0a94dfd28d8e7b62be9ad3852bdbd72571cc6c72
Gil Pratte <gilpratte@Gils-MacBook-Pro.local>
2016-09-06 13:47:22 -0500

    comment the singleton code

--- src/Single.java
+++ src/Single.java
@@ -2,13 +2,16 @@
  * Created by gilpratte on 9/6/16.
  */
 public class Single {
-    
+
+    // Static class variable so there is only one. Creates the one and only instance of this class.
     private static Single ourInstance = new Single();
 
+    // Public static class getter method to get the one and only one instance
     public static Single getInstance() {
         return ourInstance;
     }
 
+    // Private constructor so no other code can "new" this class into an object
     private Single() {
     }
 }
